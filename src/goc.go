2f3a7b7461b1d580fce5117fc4f8d193dfa1e2d7
linbing <840412413@qq.com>
2015-04-09 14:39:49 +0800

    go call c

--- src/goc.go
+++ src/goc.go
@@ -1,14 +1,33 @@
 package main
 
 /*
+#include <stdio.h>
 #include <stdlib.h>
+#if 1
+void hello()
+{
+	printf("hello world \n");
+//return;
+}
+#endif
 */
 import "C"
 import (
 	"fmt"
+	"unsafe"
 )
 
+func Prints(s string) {
+	p := C.CString(s)
+	C.fputs(p, (*C.FILE)(C.stdout))
+	C.free(unsafe.Pointer(p))
+}
 func start_goc() {
-	fmt.Println("-------------")
-	C.printf("c hello world \n")
+	fmt.Println("------start goc-------")
+	//fmt.Println(int(C.random()))
+	//C.hello()
+	//C.printf("in c hello world \n");//error : unexpected type: ...
+	s := "linbing"
+	Prints(s)
+
 }
