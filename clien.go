ed9fb74b5731a0d7f695d7bc5e36d514ff6dca13
linbing <840412413@qq.com>
2015-04-09 18:10:42 +0800

    add client

---
+++ clien.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+const RECV_BUF_LEN = 1024
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:6666")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	buf := make([]byte, RECV_BUF_LEN)
+
+	for i := 0; i < 5; i++ {
+		msg := fmt.Sprintf("hello world , %03d", i)
+		n, err := conn.Write([]byte(msg))
+		if err != nil {
+			fmt.Printf("write bugger error:", err.Error())
+			break
+		}
+		fmt.Println(msg)
+
+		n, err = conn.Read(buf)
+		if err != nil {
+			fmt.Println("read buffer error :", err.Error())
+			break
+		}
+		fmt.Println(string(buf[0:n]))
+		time.Sleep(time.Second)
+	}
+}
